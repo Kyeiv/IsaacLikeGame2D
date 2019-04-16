b44bc1e04d83ea2c311a52b2ddd9faf4fedd715b
Kyeiv <krzy.poloczek@gmail.com>
2019-04-16 21:42:40 +0200

    The Devil commit!

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        Debug.Log("COLLIDED");
+        collison.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+
+    }
+}
