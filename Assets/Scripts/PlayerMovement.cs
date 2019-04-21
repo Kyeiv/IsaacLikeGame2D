0ba05d86004ee151479467066e04b84744fd2f39
przemo029 <przemol1111@o2.pl>
2019-04-21 13:43:46 +0200

    add shooting with bullet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,15 @@ public class PlayerMovement : MonoBehaviour {
     Rigidbody2D rbody;
     Animator anim;
 
+    public GameObject bulletToRight, bulletToLeft, bulletToUp, bulletToDown;
+    Vector2 bulletPos;
+    public float fireRate = 0.5f;
+    float nextFire = 0;
+    bool facingRight = false;
+    bool facingLeft = false;
+    bool facingUp = false;
+    bool facingDown = false;
+
 	// Use this for initialization
 	void Start () {
         rbody = GetComponent<Rigidbody2D>();
@@ -23,7 +32,27 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetBool("iswalking", true);
             anim.SetFloat("input_x", movement_vector.x);
             anim.SetFloat("input_y", movement_vector.y);
-
+            if (movement_vector.x > 0){
+                facingRight = true;
+                facingLeft = false;
+                facingUp = false;
+                facingDown = false;
+            } else if (movement_vector.x < 0){
+                facingRight = false;
+                facingLeft = true;
+                facingUp = false;
+                facingDown = false;
+            } else if(movement_vector.y > 0){
+                facingRight = false;
+                facingLeft = false;
+                facingUp = true;
+                facingDown = false;
+            } else {
+                facingRight = false;
+                facingLeft = false;
+                facingUp = false;
+                facingDown = true;
+            }
         }
         else
         {
@@ -32,6 +61,32 @@ public class PlayerMovement : MonoBehaviour {
 
         rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime);
 
-		
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            fire();
+        }
 	}
+
+    void fire() {
+        if (facingRight)
+            bulletPos = transform.position + new Vector3(-0.5f, 0f, 0f);
+        else if (facingLeft)
+            bulletPos = transform.position + new Vector3(0.5f, 0f, 0f);
+
+        bulletPos = transform.position;
+
+        if (facingRight) {
+            bulletPos += new Vector2(+1f, 0f);
+            Instantiate(bulletToRight, bulletPos, Quaternion.identity);
+        } else if (facingLeft){
+            bulletPos += new Vector2(-1f, 0f);
+            Instantiate(bulletToLeft, bulletPos, Quaternion.identity);
+        } else if (facingUp){
+            bulletPos += new Vector2(0f, +1f);
+            Instantiate(bulletToUp, bulletPos, Quaternion.identity);
+        } else { 
+            bulletPos += new Vector2(0f, -1f);
+            Instantiate(bulletToDown, bulletPos, Quaternion.identity);
+        }
+    }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    public float velX = 5f;
+    public float velY = 0f;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        rb.velocity = new Vector2(velX, velY);
     }
 }
