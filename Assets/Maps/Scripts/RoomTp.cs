247675a3eca976b00d7011df42980fc1d1fb0745
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-05-18 22:37:17 +0200

    to samo ale inaczej

---
+++ Assets/Maps/Scripts/RoomTp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTp : MonoBehaviour
+{
+
+    public Vector2 shift;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            other.gameObject.transform.position += new Vector3(shift.x, shift.y, 0f);
+        }
+    }
+}
