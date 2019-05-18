44eb3d9c0574eaf6dcf9931211e3260bc91c92e1
szymszp563 <szymon.szpakowski6@gmail.com>
2019-05-18 21:29:29 +0200

    teleports

--- Assets/Maps/Scripts/TutorialMovement.cs
+++ Assets/Maps/Scripts/TutorialMovement.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class TutorialMovement : MonoBehaviour {
 	Vector3 moveJump = Vector2.zero;
 	float horMove, vertMove;
-	void Start(){
-		SheetAssigner SA = FindObjectOfType<SheetAssigner>();
+    GameObject player;//added
+    public GameObject enter; //added
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");//added
+        enter = GameObject.FindGameObjectWithTag("DoorTrigger");//added
+        SheetAssigner SA = FindObjectOfType<SheetAssigner>();
 		Vector2 tempJump = SA.roomDimensions + SA.gutterSize;
 		moveJump = new Vector3(tempJump.x, tempJump.y, 0); //distance b/w rooms: to be used for movement
 	}
 	void Update()
 	{
-		if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || 
-			Input.GetKeyDown("a") || Input.GetKeyDown("d")) //if any 'wasd' key is pressed
-		{
+		if ((Input.GetKeyDown("up") || Input.GetKeyDown("s") || 
+			Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+            &&enter.transform.position.x==player.transform.position.x//added
+            && enter.transform.position.y == player.transform.position.y)//added //if any 'wasd' key is pressed
+        {
 			horMove = System.Math.Sign(Input.GetAxisRaw("Horizontal"));//capture input
 			vertMove = System.Math.Sign(Input.GetAxisRaw("Vertical"));
 			Vector3 tempPos = transform.position;
 			tempPos += Vector3.right * horMove * moveJump.x; //jump bnetween rooms based opn input
 			tempPos += Vector3.up * vertMove * moveJump.y;
-			transform.position = tempPos;
+            
+            player.transform.position = tempPos;//added
+            transform.position = tempPos;
 		}
 	}
 }
