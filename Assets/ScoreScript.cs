a7a8323a41205eec737dd0f91ca531625dec2309
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-06-19 00:43:11 +0200

    Przemo dodaj boola

--- Assets/ScoreScript.cs
+++ Assets/ScoreScript.cs
@@ -7,11 +7,14 @@ public class ScoreScript : MonoBehaviour
 {
     private Text text;
     public int score=0;
+    int time;
+    bool CZY_BOSS_ZABITY;
     // Start is called before the first frame update
     void Start()
     {
         score = GameObject.FindGameObjectWithTag("GameController").GetComponent<UtilityBehaviors>().getScore();
         GameObject.FindGameObjectWithTag("GameController").GetComponent<UtilityBehaviors>().resetScore();
+        time = (int)GameObject.FindGameObjectWithTag("GameController").GetComponent<Timer>().getTime();
         GameObject.FindGameObjectWithTag("GameController").GetComponent<Timer>().resetTimer();
         text = gameObject.GetComponent<Text>();   
     }
@@ -19,7 +22,7 @@ public class ScoreScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = "Your score " + score.ToString();
+        text.text = "Your score " + (((300 - time) > 0 && CZY_BOSS_ZABITY) ? (score + 300 - time).ToString() : score.ToString());
         GameObject.FindGameObjectWithTag("GameController").GetComponent<Timer>().resetTimer();
     }
 }
