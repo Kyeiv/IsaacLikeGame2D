4908c3fdc022d424d7ab852bdd2fb081fd96bd37
szymszp563 <szymon.szpakowski6@gmail.com>
2019-06-18 23:15:05 +0200

    added our own music form neural network

---
+++ Assets/SoundManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public AudioClip backgroundSound;
+    public AudioSource audioSrc;
+    public bool mute = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc.clip = backgroundSound;
+        //backgroundSound = Resources.Load<AudioClip>("music");
+        //audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mute = !mute;
+        }
+        if (!mute)
+        {
+            audioSrc.Play();
+        }
+        
+    }
+
+    /*public static void PlaySound()
+    {
+        audioSrc.PlayOneShot(backgroundSound);
+    }*/
+}
