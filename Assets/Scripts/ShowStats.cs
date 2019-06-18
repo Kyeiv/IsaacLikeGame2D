b2051390b5a3c2ce896af9c16d5e8a579c56e223
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-06-19 00:28:46 +0200

    wyswietlanie statystyk

---
+++ Assets/Scripts/ShowStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowStats : MonoBehaviour
+{
+    
+    UtilityBehaviors stats;
+    Timer timer;
+    Text text;
+    
+    void Start()
+    {
+        stats = GameObject.FindGameObjectWithTag("GameController").GetComponent<UtilityBehaviors>();
+        timer = GameObject.FindGameObjectWithTag("GameController").GetComponent<Timer>();
+        text = GetComponent<Text>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Score: " + stats.getScore() + "       Seconds: " + (int)timer.getTime();
+    }
+}
