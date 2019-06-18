356d0c415ce73ed1a31071d9fbe8050f6c8cc446
przemo029 <przemol1111@o2.pl>
2019-06-19 00:31:09 +0200

    added end window after killed boss

--- Assets/Scripts/BulletCollision.cs
+++ Assets/Scripts/BulletCollision.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class BulletCollision : MonoBehaviour
 {
@@ -44,6 +46,7 @@ public class BulletCollision : MonoBehaviour
             {
                 scoreScript.addScore(100);
                 script.currentState = EnemyState.Die;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
 
